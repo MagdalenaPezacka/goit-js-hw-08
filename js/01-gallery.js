19c01cb61aa276b5caa18170fe219093d1c4d3ca
MagdalenaPezacka <magdalena.pezacka.it@gmail.com>
2023-09-01 21:48:12 +0200

    firstStep

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,20 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+const fotoGallery = document.querySelector(".gallery");
+const getFotoGallery = galleryItems
+	.map(
+		({preview, original, description}) => `<li class="gallery__item"><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+	</li>`
+	)
+	.join(" ");
+
+fotoGallery.insertAdjacentHTML('beforeend', getFotoGallery)
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionPosition: 'outside', captionDelay: 250, nav: false, close: false, showCounter: false });
 console.log(galleryItems);
