629ccd61b87cedc75b6f7bd7772e451f8ca9390b
MagdalenaPezacka <magdalena.pezacka.it@gmail.com>
2023-09-03 21:49:04 +0200

    task3update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,7 +4,9 @@ const form = document.querySelector('.feedback-form');
 const email = document.querySelector('input[name="email"]');
 const message = document.querySelector('textarea[name="message"]');
 const LOCALSTORAGE_KEY = 'feedback-form-state';
+let data = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
 
+onReload();
 //zapisuje w localStorage obiekt z wartościami pól email i message//
 const formRefill = () => {
   const refill = {
@@ -14,22 +16,27 @@ const formRefill = () => {
   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(refill));
 };
 form.addEventListener('input', throttle(formRefill, 500));
+
 // form.addEventListener('input', formRefill);
 
 //po przeładowywaniu strony, jeśli w localStorage są dane - wypełnia nimi pola
 //formularza, jeśli nie - pola puste//
-
-let data = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-if (localStorage) {
-  email.value = data.email;
-  message.value = data.message;
+function onReload() {
+  
+  if (data) {
+    email.value = data.email;
+    message.value = data.message;
+  }
 }
-
 //po wysłaniu formularza czyści storage i pola formularza, a także wyloguj obiekt
 //z polami email, message i ich aktualnymi wartościami do konsoli
 
 function onSubmit(event) {
   event.preventDefault();
+  
+  if (email.value === '' || message.value === '') {
+    return alert("Please fill in all the fields!");
+  }
   localStorage.setItem(LOCALSTORAGE_KEY, email.value && message.value);
   updateInput();
   form.reset();
@@ -40,3 +47,4 @@ function updateInput() {
 }
 
 form.addEventListener('submit', onSubmit);
+console.log({ email: email.value, message: message.value });
